378bf4640c3903c4db2c01b815e8ab84723a18f8
Fujale Ahmed Shaik <shaikfujale.ahmed@cognizant.com>
2024-01-22 11:57:37 +0000

    294755 - Added decorators to benefit from TypeScript typings

--- src/infrastructure/plugins/shared_data.ts
+++ src/infrastructure/plugins/shared_data.ts
@@ -1,17 +1,30 @@
 import { Server } from '@hapi/hapi';
 
+interface ShareData {
+  [key: string]: null;
+}
+
+interface ServerWithDecorators extends Server {
+  updateSharedData: (key: string, value: null) => void;
+  purgeSharedData: (key: string) => void;
+}
+
 module.exports = {
   plugin: 'SharedData',
-  register: (server: Server) => {
-    const sharedData: { [key: string]: null } = {};
+  register: (server: ServerWithDecorators) => {
+    const sharedData: ShareData = {};
 
     server.expose('getSharedData', () => sharedData);
 
-    server.decorate('server', 'updateSharedData', (key, value) => {
-      sharedData[key] = value;
-    });
+    server.decorate(
+      'server',
+      'updateSharedData',
+      (key: string, value: null) => {
+        sharedData[key] = value;
+      }
+    );
 
-    server.decorate('server', 'purgeSharedData', (key) => {
+    server.decorate('server', 'purgeSharedData', (key: string) => {
       delete sharedData[key];
     });
   },
