b52eccf8f99d5002a3a99748a9af1f51c4ff22c7
Karthigayayini Ramachandran <karthigayayini.r@cognizant.com>
2024-01-29 00:17:51 +0000

    fix: Unit test cases

--- tests/controllers/web/SearchResultsController.spec.ts
+++ tests/controllers/web/SearchResultsController.spec.ts
@@ -3,6 +3,10 @@
 import { mock } from 'jest-mock-extended';
 import { Request, ResponseToolkit } from '@hapi/hapi';
 import { SearchResultsController } from '../../../src/controllers/web/SearchResultsController';
+import { fromDate, toDate } from '../../../src/views/forms/dateQuestionnaireFields';
+import { dateQuestionChronologicalError, dateQuestionnaireGovUKError } from '../../data/dateQuestionnaire';
+import { DateQuestionnaireError } from '../../../src/interfaces/guidedSearch';
+import * as errorTransformer from '../../../src/utils/transformErrors';
 
 describe('Search Results Controller > deals with rendering search results handler', () => {
   const mockRequest = mock<Request>();
@@ -20,5 +24,39 @@ describe('Search Results Controller > deals with rendering search results handle
     expect(mockResponse.view).toHaveBeenCalledWith('screens/results/template', {
       searchTerm: mockRequest.query?.q,
     });
+  });
+});
+
+describe('Search Results Controller > renderGuidedSearchHandler', () => {
+  const mockRequest = mock<Request>();
+  const mockResponse = mock<ResponseToolkit>();
+
+  beforeAll(() => {
+    return SearchResultsController.renderGuidedSearchHandler(
+      mockRequest,
+      mockResponse
+    );
+  });
+
+  it('should render the date questionnaire template', async () => {
+    expect(mockResponse.view).toHaveBeenCalledWith('screens/guided_search/date_questionnaire', {
+      fromDate, toDate
+    });
+  });
+});
+
+describe('Search Results Controller > guidedSearchFailActionHandler', () => {
+  const h = {
+    view: jest.fn().mockReturnThis(),
+    takeover: jest.fn()
+  };
+  const error = {};
+  jest.spyOn(errorTransformer,'transformErrors').mockReturnValue(dateQuestionChronologicalError as DateQuestionnaireError);
+  beforeAll(() => {
+    return SearchResultsController.guidedSearchFailActionHandler(h, error);
+  });
+
+  it('should render the date questionnaire template with error messages', async () => {
+    expect(h.view).toHaveBeenCalledWith('screens/guided_search/date_questionnaire', dateQuestionnaireGovUKError);
   });
 });
