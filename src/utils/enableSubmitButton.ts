e4ea7887452aee94ddd4f68aed54a15c9d01ef3b
Fujale Ahmed Shaik <shaikfujale.ahmed@cognizant.com>
2024-02-02 02:09:12 +0000

    294976 - Fixed sonar issue

--- src/utils/enableSubmitButton.ts
+++ src/utils/enableSubmitButton.ts
@@ -3,10 +3,10 @@ import { IFormValidatorOptions } from '../interfaces/guidedSearch.interface';
 const enableSubmitButton = async (formValidatorOptions: IFormValidatorOptions) => {
   const form = document.getElementById(formValidatorOptions.formId) as HTMLFormElement;
   const submitButton = document.getElementById(formValidatorOptions.submitButtonId) as HTMLButtonElement;
-  const fields = Array.from(form.querySelectorAll('input[type="text"], input[type="number"]')) as HTMLInputElement[];
+  const fields: HTMLInputElement[] = Array.from(form.querySelectorAll('input[type="text"], input[type="number"]'));
 
   const checkFields = (): void => {
-    const hasValue = fields.some((field) => field.value.trim() !== '');
+    const hasValue = fields.some((field) => field?.value.trim() !== '');
     submitButton.disabled = !hasValue;
   };
 
