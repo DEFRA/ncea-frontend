ffa8f462ba4ea99700b035e5b634bbef75f634ba
Fujale Ahmed Shaik <shaikfujale.ahmed@cognizant.com>
2024-02-04 15:16:37 +0000

    294755 - Deleted unnecessary sample route, controllers and spec file

--- src/infrastructure/plugins/router.ts
+++ src/infrastructure/plugins/router.ts
@@ -7,8 +7,7 @@ const routes = [].concat(
   require('../../routes/web/assets'),
   require('../../routes/web/home'),
   require('../../routes/web/search'),
-  require('../../routes/web/sample'),
-  require('../../routes/api/api'),
+  require('../../routes/api/api')
 );
 
 module.exports = {
