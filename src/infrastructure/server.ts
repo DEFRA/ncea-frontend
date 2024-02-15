b91fb98293add42d9583c43850a1118f79b5cbb4
CTS\189285 <Janardhanan.SusaiManiPeter@cognizant.com>
2024-02-15 12:55:39 +0100

    Added docker file

--- src/infrastructure/server.ts
+++ src/infrastructure/server.ts
@@ -10,55 +10,34 @@ const { AzureApplicationInsightsLogger } = require('winston-azure-application-in
 
 const shouldPushToAppInsights = 'SHOULD_PUSH_TO_APPINSIGHTS' in process.env;
 
-if (shouldPushToAppInsights) {
-    // appInsights
-    //   .setup("InstrumentationKey=beb07cdc-ed03-493a-88e3-ce52a5db8a99;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com")
-    //   .enableWebInstrumentation(true)
-    //   .setAutoDependencyCorrelation(true)
-    //   .setAutoCollectRequests(true)
-    //   .setAutoCollectPerformance(true)
-    //   .setAutoCollectExceptions(true)
-    //   .setAutoCollectDependencies(true)
-    //   .setAutoCollectConsole(true, true)
-    //   .setUseDiskRetryCaching(true)
-    //   .setSendLiveMetrics(true)
-    //   .setInternalLogging(false, true)
-    //   .setAutoCollectHeartbeat(true)
-    //   .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
-    // appInsights.defaultClient.setAutoPopulateAzureProperties()
-    // appInsights.start();
-  getSecret("ApplicationInsights--ConnectionString")
-  .then(appInsightsConnectionString  => {
-    appInsights
-      .setup(appInsightsConnectionString)
-      .enableWebInstrumentation(true)
-      .setAutoDependencyCorrelation(true)
-      .setAutoCollectRequests(true)
-      .setAutoCollectPerformance(true)
-      .setAutoCollectExceptions(true)
-      .setAutoCollectDependencies(true)
-      .setAutoCollectConsole(true, true)
-      .setUseDiskRetryCaching(true)
-      .setSendLiveMetrics(true)
-      .setInternalLogging(false, true)
-      .setAutoCollectHeartbeat(true)
-      .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
-    appInsights.defaultClient.setAutoPopulateAzureProperties()
-    appInsights.start();
+if (shouldPushToAppInsights) {  
+  appInsights
+  .setup("InstrumentationKey=beb07cdc-ed03-493a-88e3-ce52a5db8a99;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/")
+  .enableWebInstrumentation(true)
+  .setAutoDependencyCorrelation(true)
+  .setAutoCollectRequests(true)
+  .setAutoCollectPerformance(true)
+  .setAutoCollectExceptions(true)
+  .setAutoCollectDependencies(true)
+  .setAutoCollectConsole(true, true)
+  .setUseDiskRetryCaching(true)
+  .setSendLiveMetrics(true)
+  .setInternalLogging(false, true)
+  .setAutoCollectHeartbeat(true)
+  .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
+appInsights.defaultClient.setAutoPopulateAzureProperties()
+appInsights.start();
 
-    // Use an existing app insights SDK instance
-    winston.add(new AzureApplicationInsightsLogger({
-      insights: appInsights
-   }));
-    console.log('Initialized app insights');
-  })
-  .catch(e => {
-    throw new Error('Cannot initialise app insights, reason: ' + e);
-  });   
+// Use an existing app insights SDK instance
+winston.add(new AzureApplicationInsightsLogger({
+  insights: appInsights
+}));
+console.log('Initialized app insights');
 } else {
     winston.add(new winston.transports.Console());
 }
 global.logger = winston;
+
 
 // Create the hapi server
 const server: Server = Hapi.server({
@@ -80,11 +59,12 @@ const initializeServer = async (): Promise<Server> => {
   // Register the custom plugins
   await server.register([require('./plugins/views'), require('./plugins/router'), require('./plugins/logger')]);
 
+  const appInsightsConnectionString = await getSecret("ApplicationInsights--ConnectionString");  
   await server.initialize();
   return server;
 };
 
-const startServer = async (): Promise<Server> => {
+const startServer = async (): Promise<Server> => {  
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);  
   return server;
