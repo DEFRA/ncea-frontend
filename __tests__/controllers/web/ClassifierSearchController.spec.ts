edf3075b7cf98b3c4da8c226370c6915c34cd710
Priti Bandewar <priti.bandewar@whitbread.com>
2024-07-24 08:49:06 +0100

    Resolved sonar query

--- __tests__/controllers/web/ClassifierSearchController.spec.ts
+++ __tests__/controllers/web/ClassifierSearchController.spec.ts
@@ -2,17 +2,11 @@
 
 import { ClassifierSearchController } from '../../../src/controllers/web/ClassifierSearchController';
 import { Request, ResponseToolkit } from '@hapi/hapi';
-import {
-  formIds,
-  webRoutePaths,
-  queryParamKeys,
-  guidedSearchSteps,
-} from '../../../src/utils/constants';
+import { formIds, guidedSearchSteps, queryParamKeys, webRoutePaths } from '../../../src/utils/constants';
 import { level3ClassifierItems } from '../../data/classifierSearch';
 import { getClassifierThemes } from '../../../src/services/handlers/classifierApi';
 import { getSearchResultsCount } from '../../../src/services/handlers/searchApi';
-import { ISearchPayload } from '../../../src/interfaces/queryBuilder.interface';
-import { generateCountPayload, upsertQueryParams } from '../../../src/utils/queryStringHelper';
+import { generateCountPayload, readQueryParams, upsertQueryParams } from '../../../src/utils/queryStringHelper';
 
 jest.mock('../../../src/services/handlers/classifierApi', () => ({
   getClassifierThemes: jest.fn(),
@@ -24,64 +18,122 @@ jest.mock('../../../src/services/handlers/searchApi', () => ({
 
 describe('Classifier Search Controller', () => {
   describe('renderClassifierSearchHandler', () => {
-    it('should call the classifier view with context and count', async () => {
-      const request: Request = { query: { level: '3', 'parent[]': ['lv2-001', 'lv2-002'] } } as any;
-      const response: ResponseToolkit = { view: jest.fn() } as any;
-      const { guidedClassifierSearch: guidedClassifierSearchPath, guidedDateSearch: skipPath, results } = webRoutePaths;
+    let request: Request;
+    let response: ResponseToolkit;
+    const { guidedClassifierSearch: guidedClassifierSearchPath, guidedDateSearch: skipPath, results, intermediate } = webRoutePaths;
+    const { classifierSearch } = formIds;
+
+    beforeEach(() => {
+      response = { view: jest.fn() } as any;
+    });
+
+    it('should call the classifier view with context when level is greater than or equal to 1 and count is 0', async () => {
+      request = {
+        query: { level: '3', 'parent[]': 'lv2-001,lv2-002' },
+      } as any;
 
       (getClassifierThemes as jest.Mock).mockResolvedValue(level3ClassifierItems);
-      (getSearchResultsCount as jest.Mock).mockResolvedValue({ totalResults: 123 });
+      (getSearchResultsCount as jest.Mock).mockResolvedValue({ totalResults: 0 });
 
       await ClassifierSearchController.renderClassifierSearchHandler(request, response);
 
-      const queryParamsObject: Record<string, string> = {
+      const payloadQuery = {
+        level: '2',
+        'parent[]': 'lv2-001,lv2-002',
+      };
+
+      const queryParamsObject = {
         [queryParamKeys.journey]: 'gs',
         [queryParamKeys.level]: '2',
-        [queryParamKeys.parent]: ['lv2-001', 'lv2-002'].join(','),
+        [queryParamKeys.parent]: 'lv2-001,lv2-002',
       };
+
       const queryString = upsertQueryParams(request.query, queryParamsObject, false);
-      const resultPathQueryString = upsertQueryParams({ ...request.query, level: '2' }, {}, true);
-      const newSkipPath = `${webRoutePaths.intermediate}/${guidedSearchSteps.classifierSearch}?${queryString}`;
-      const resultsPath = `${results}?${resultPathQueryString}`;
+      const resultsPath = `${results}?${readQueryParams(payloadQuery, '', true)}`;
+      const skipPathUrl = `${intermediate}/${guidedSearchSteps.classifierSearch}?${queryString}`;
 
       expect(response.view).toHaveBeenCalledWith('screens/guided_search/classifier_selection.njk', {
         guidedClassifierSearchPath,
-        nextLevel: '4',
-        skipPath: newSkipPath,
-        formId: formIds.classifierSearch,
+        nextLevel: "4",
+        skipPath: skipPathUrl,
+        formId: classifierSearch,
         classifierItems: level3ClassifierItems,
-        count: '123',
+        count: '0',
         resultsPath,
         backLinkPath: '#',
         backLinkClasses: 'back-link-classifier',
       });
     });
 
-    it('should call the classifier view with context without count', async () => {
-      const request: Request = { query: { level: '0', 'parent[]': ['lv2-001', 'lv2-002'] } } as any;
-      const response: ResponseToolkit = { view: jest.fn() } as any;
-      const { guidedClassifierSearch: guidedClassifierSearchPath, guidedDateSearch: skipPath, results } = webRoutePaths;
+    it('should call the classifier view with context when level is less than 1', async () => {
+      request = {
+        query: { level: '0', 'parent[]': '' },
+      } as any;
 
       (getClassifierThemes as jest.Mock).mockResolvedValue(level3ClassifierItems);
 
       await ClassifierSearchController.renderClassifierSearchHandler(request, response);
 
-      const queryParamsObject: Record<string, string> = {
+      const payloadQuery = {
+        level: '-1',
+        'parent[]': '',
+      };
+
+      const queryParamsObject = {
         [queryParamKeys.journey]: 'gs',
         [queryParamKeys.level]: '-1',
-        [queryParamKeys.parent]: ['lv2-001', 'lv2-002'].join(','),
+        [queryParamKeys.parent]: '',
       };
+
       const queryString = upsertQueryParams(request.query, queryParamsObject, false);
-      const resultPathQueryString = upsertQueryParams({ ...request.query, level: '-1' }, {}, true);
-      const resultsPath = `${results}?${resultPathQueryString}`;
+      const resultsPath = `${results}?${readQueryParams(payloadQuery, '', true)}`;
+      const skipPathUrl = "/date-search";
 
       expect(response.view).toHaveBeenCalledWith('screens/guided_search/classifier_selection.njk', {
         guidedClassifierSearchPath,
-        nextLevel: '1',
-        skipPath,
-        formId: formIds.classifierSearch,
+        nextLevel: "1",
+        skipPath: skipPathUrl,
+        formId: classifierSearch,
         classifierItems: level3ClassifierItems,
         count: null,
+        resultsPath,
+        backLinkPath: '#',
+        backLinkClasses: 'back-link-classifier',
+      });
+    });
+
+    it('should call the classifier view with context when count is present', async () => {
+      request = {
+        query: { level: '3', 'parent[]': 'lv2-001,lv2-002' },
+      } as any;
+
+      (getClassifierThemes as jest.Mock).mockResolvedValue(level3ClassifierItems);
+      (getSearchResultsCount as jest.Mock).mockResolvedValue({ totalResults: 10 });
+
+      await ClassifierSearchController.renderClassifierSearchHandler(request, response);
+
+      const payloadQuery = {
+        level: '2',
+        'parent[]': 'lv2-001,lv2-002',
+      };
+
+      const queryParamsObject = {
+        [queryParamKeys.journey]: 'gs',
+        [queryParamKeys.level]: '2',
+        [queryParamKeys.parent]: 'lv2-001,lv2-002',
+      };
+
+      const queryString = upsertQueryParams(request.query, queryParamsObject, false);
+      const resultsPath = `${results}?${readQueryParams(payloadQuery, '', true)}`;
+      const skipPathUrl = `${intermediate}/${guidedSearchSteps.classifierSearch}?${queryString}`;
+
+      expect(response.view).toHaveBeenCalledWith('screens/guided_search/classifier_selection.njk', {
+        guidedClassifierSearchPath,
+        nextLevel: "4",
+        skipPath: skipPathUrl,
+        formId: classifierSearch,
+        classifierItems: level3ClassifierItems,
+        count: '10',
         resultsPath,
         backLinkPath: '#',
         backLinkClasses: 'back-link-classifier',
