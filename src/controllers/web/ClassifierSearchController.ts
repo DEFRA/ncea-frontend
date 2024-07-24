7b2defa91f75e874c062e9b712d91be5d9a092ee
Priti Bandewar <priti.bandewar@whitbread.com>
2024-07-24 07:45:35 +0100

    Modified the failed test cases

--- src/controllers/web/ClassifierSearchController.ts
+++ src/controllers/web/ClassifierSearchController.ts
@@ -1,6 +1,5 @@
 'use strict';
 
-import { ISearchPayload } from '../../interfaces/queryBuilder.interface';
 import { getClassifierThemes } from '../../services/handlers/classifierApi';
 import { getSearchResultsCount } from '../../services/handlers/searchApi';
 import { Request, ResponseObject, ResponseToolkit } from '@hapi/hapi';
@@ -9,63 +8,53 @@ import { generateCountPayload, readQueryParams, upsertQueryParams } from '../../
 
 const ClassifierSearchController = {
   renderClassifierSearchHandler: async (request: Request, response: ResponseToolkit): Promise<ResponseObject> => {
-    const { guidedClassifierSearch: guidedClassifierSearchPath, guidedDateSearch: skipPath, results } = webRoutePaths;
+    const {
+      guidedClassifierSearch: guidedClassifierSearchPath,
+      guidedDateSearch: skipPath,
+      results,
+      intermediate
+    } = webRoutePaths;
     const formId: string = formIds.classifierSearch;
-    const level: string = readQueryParams(request.query, 'level');
-    const parent: string = readQueryParams(request.query, 'parent[]');
-    const nextLevel: string = (+level + 1).toString();
+    const level: number = Number(readQueryParams(request.query, 'level'));
+    const parent: string = readQueryParams(request.query, 'parent[]') || '';
+    const nextLevel: string = (level + 1).toString();
+
     const payloadQuery = {
       ...request.query,
-      level: (Number(level) - 1).toString(),
+      level: (level - 1).toString(),
     };
-    const resultPathQueryString: string = readQueryParams(payloadQuery, '', true);
+
     const queryParamsObject: Record<string, string> = {
       [queryParamKeys.journey]: 'gs',
-      [queryParamKeys.level]: (Number(level) - 1).toString() ?? '',
-      [queryParamKeys.parent]: parent ?? [],
+      [queryParamKeys.level]: (level - 1).toString(),
+      [queryParamKeys.parent]: parent,
     };
+
     const queryString: string = upsertQueryParams(request.query, queryParamsObject, false);
-    const resultsPath: string = `${results}?${resultPathQueryString}`;
+    const resultsPath: string = `${results}?${readQueryParams(payloadQuery, '', true)}`;
+    const skipPathUrl: string = `${intermediate}/${guidedSearchSteps.classifierSearch}?${queryString}`;
+
+    const classifierItems = await getClassifierThemes(level.toString(), parent);
 
     const renderView = async (showCount: boolean) => {
-      let count: string | null = null;
-      if (showCount) {
-        const queryBuilderSearchObject: ISearchPayload = generateCountPayload(payloadQuery);
-        const searchResultsCount: { totalResults: number } = await getSearchResultsCount(queryBuilderSearchObject);
-        count = searchResultsCount.totalResults.toString() || null;
-        const newSkipPath: string = `${webRoutePaths.intermediate}/${guidedSearchSteps.classifierSearch}?${queryString}`;
-        const classifierItems = await getClassifierThemes(level, parent);
-        return response.view('screens/guided_search/classifier_selection.njk', {
-          guidedClassifierSearchPath,
-          nextLevel,
-          skipPath: newSkipPath,
-          formId,
-          classifierItems,
-          count,
-          resultsPath,
-          backLinkPath: '#',
-          backLinkClasses: 'back-link-classifier',
-        });
-      } else {
-        const classifierItems = await getClassifierThemes(level, parent);
-        return response.view('screens/guided_search/classifier_selection.njk', {
-          guidedClassifierSearchPath,
-          nextLevel,
-          skipPath,
-          formId,
-          classifierItems,
-          count,
-          resultsPath,
-          backLinkPath: '#',
-          backLinkClasses: 'back-link-classifier',
-        });
-      }
+      const count = showCount
+        ? (await getSearchResultsCount(generateCountPayload(payloadQuery))).totalResults.toString()
+        : null;
+
+      return response.view('screens/guided_search/classifier_selection.njk', {
+        guidedClassifierSearchPath,
+        nextLevel,
+        skipPath: showCount ? skipPathUrl : skipPath,
+        formId,
+        classifierItems,
+        count,
+        resultsPath,
+        backLinkPath: '#',
+        backLinkClasses: 'back-link-classifier',
+      });
     };
-    if (Number(payloadQuery.level) >= 1) {
-      return renderView(true);
-    } else {
-      return renderView(false);
-    }
+
+    return renderView(level >= 1);
   },
 };
 
